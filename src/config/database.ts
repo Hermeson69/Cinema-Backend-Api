b452c8a9d138e36e9710c564f8625cce159dfacf
Hermeson Alves <hermesonalves256@gmail.com>
2026-03-18 16:47:30 -0300

    feat: update database configuration and add test script for PostgreSQL connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,9 @@
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../db/schema";
 
-const client = postgres(process.env.DB_URL!);
+const client = postgres(process.env.DB_URL!, { prepare: false });
 
 export const db = drizzle(client, { schema });
-
 export type { schema };
